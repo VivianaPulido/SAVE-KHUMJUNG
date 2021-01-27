7a6c326c39e8082a1229b41c736ac714b4bca1de
Viviana Pulido <vivianapulido_dg@outlook.com>
2021-01-27 15:24:12 -0600

    funcionando js, sigue arreglar html y css

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const canvas= document.getElementById("canvas");
 const ctx= canvas.getContext("2d");
 canvas.width = 900
 canvas.height = 560
+const yMitad= canvas.height/2
+const yInicial = canvas.height - canvas.height
+const xInicial = canvas.width - canvas.width
+//DOM
 const huntersScoreHTML = document.querySelector(".huntersScore")
 const timeHtml= document.querySelector(".countTime")
 const botonComprar= document.getElementById("comprar")
+const botonStart = document.querySelector(".btnStartGame")
+const botonRestart = document.getElementsByClassName("btnRestarttGame")
+const gameOver = document.querySelector("gameOver")
+const yetiLiveCount = document.querySelector("livesCount")
+const coinsCountHtml = document.querySelector(".coinsCounter")
+const aldeanosCountHtml= document.querySelector(".aldeanosCount")
 let lastTime //REVISAR 
 const updateTime= (()=>{
     let now= Date.now()
     let dt= (now-lastTime)/1000
     updateCanvas(dt)
-
 })
-
 class Ground {
     constructor(x, y, width, height){
         this.x= x;
@@ -29,7 +36,6 @@ class Ground {
         ctx.drawImage(this.imgSource, this.x, this.y, this.width, this.height)
     }
 }
-
 const image= new Image()
 class Component {
     constructor(x, y, width, height, imgSource){
@@ -47,14 +53,29 @@ class Player extends Component {
     this.playerImg.src= "./imagenes/yeti.png"
     this.playerImg.onload= () => {
     this.draw()
+    this.vida = 2
   }
     }
     draw(){
         ctx.drawImage(this.playerImg, this.x, this.y, this.width, this.height)
     }
+    moveUp(){ ///revisar error que aparece, no afeta en jugabilidad pero hay que revisarlo
+        if(this.y <= yInicial+5) {
+          y=(canvas.height - canvas.height)  + this.height 
+        } else {
+        this.y-=25 
+        }
+    }
+    moveDown(){
+        if(this.y+this.height >= canvas.height-10) {
+            y=(canvas.height-this.height)
+          } else {
+          this.y+=25 
+          }
+    }
 }
 class Enemy extends Component {
-    constructor(x, y, width, height, imgSource, velocity, strength){
+    constructor(x, y, width, height, imgSource, velocity, strength, name){
         super(x, y, width, height, imgSource)
         this.velocity= velocity;
         this.enemyImg= new Image()
@@ -63,11 +84,10 @@ class Enemy extends Component {
             this.draw()
          }
         this.strength= strength;
+        this.name= name;
         }
         draw(){
             ctx.drawImage(this.enemyImg, this.x, this.y, this.width, this.height)
-        }
-        move(){
         }
         updateEnemy(){
             this.x = this.x + this.velocity.x
@@ -81,26 +101,26 @@ class Shooter extends Component {
     this.playerImg.src= "./imagenes/bigfoot.png"
     this.playerImg.onload= () => {
     this.draw()
-}
-this.vida = 5
+    }
+    this.vida = 5
 }
 draw(){
     ctx.drawImage(this.playerImg, this.x, this.y, this.width, this.height)
 }}
-class Proyectiles {
-    constructor(x, y, radio, color, velocity){
-    this.x = x;
-    this.y = y;
-    this.radio= radio;
-    this.color= color;
-    this.velocity= velocity;
+class Proyectiles extends Component {
+    constructor(x, y,imgSource, velocity){
+        super(x, y, imgSource) //cuando usas exted y el width es doferente hay que ponerlo en el super??
+        this.width = 20
+        this.height = 20
+        this.velocity = velocity;
+        this.imgSource= new Image()
+        this.imgSource.src= imgSource
+        this.imgSource.onload= ()=>{
+            this.draw()
+         }
     }
-    draw() {
-        ctx.beginPath()
-        ctx.arc(this.x,this.y,this.radio,0,Math.PI * 2)
-        ctx.fillStyle = this.color
-        ctx.fill()
-        ctx.closePath()
+    draw(){
+        ctx.drawImage(this.imgSource, this.x, this.y, this.width, this.height)
     }
     updateProyectil(){
         this.draw()
@@ -136,26 +156,31 @@ class Particles extends Component {
     }
 }
 
-const yMitad= canvas.height/2
-const yInicial = canvas.height - canvas.height
-const xInicial = canvas.width - canvas.width
 //Instanciammiento de jugadores y personajes
-const filasGround= [[xInicial, yInicial + 70], [xInicial, yInicial+ 140], [xInicial, yInicial+210, [xInicial, yInicial+280]]
-[xInicial, yInicial+350], [xInicial, yInicial+420], [xInicial, yInicial+490], [xInicial, yInicial+560]]
-
-let ground= new Ground(0, 0, canvas.width, canvas.height)
-let yeti1= new Player(xInicial+10, yMitad, 70, 70)
+//const filasGround= [[xInicial, yInicial + 70], [xInicial, yInicial+ 140], [xInicial, yInicial+210, [xInicial, yInicial+280]]
+//[xInicial, yInicial+350], [xInicial, yInicial+420], [xInicial, yInicial+490], [xInicial, yInicial+560]]
+let groundGame= new Ground(0, 0, canvas.width, canvas.height)
+let groundLost= new Ground(0, 0, canvas.width, canvas.height)
+let yeti1 = new Player(xInicial+10, yMitad, 70, 70)
+console.log(yeti1)
+let yetiLive= yeti1.vida
+//yetiLiveCount.innerHTML= yetiLive
+console.log(yetiLive)
 let bigFoot1 = new Shooter(xInicial+90, yInicial+ 5, 70, 70)
-let bigFoot2 = new Shooter(xInicial+90, yInicial+75, 70, 70)
-let bigFoot3 = new Shooter(xInicial+90, yInicial+145, 70, 70)
-let enemiesAmarillosArr= [];
-let enemiesRojosArr= [];
+let bigFoot2 = new Shooter(xInicial+90, yInicial+75, 70, 70) 
+let bigFoot3 = new Shooter(xInicial+90, yInicial+145, 70, 70) 
+let bigFootArr= [bigFoot1, bigFoot2, bigFoot3];
 let enemiesArr= [];
 let proyectilesArr= [];
-let bigFootArr= [bigFoot1, bigFoot2, bigFoot3];
 let particlesArray= [];
-
-
+//Funcion para arrancar con botones Restart y Start
+function init() {
+     enemiesArr= [];
+     proyectilesArr= [];
+     particlesArray= [];
+     huntersScoreHTML.innerHTML = 0
+     //gameOver.style.display="none"
+}
 //Función de generación de enemigos
 function generarEnemigoRojo(){
     setInterval(() =>{
@@ -167,12 +192,9 @@ function generarEnemigoRojo(){
             x: -2,
             y: 0,
         }
-        //enemiesAmarillosArr.push(new Enemy(x, y, width, height,"./imagenes/Cazador1.jpeg", velocity, 5)) //""
-        //enemiesArr= [...enemiesAmarillosArr]
-        enemiesArr.push(new Enemy(x, y, width, height,"./imagenes/cazador2.png", velocity, 1)) //
+        enemiesArr.push(new Enemy(x, y, width, height,"./imagenes/cazador2.png", velocity, 1, "rojo")) //
     },3000)
-}
-
+} 
 function generarEnemigoAmarillo(){
     setInterval(() =>{
         let width= 70
@@ -183,10 +205,9 @@ function generarEnemigoAmarillo(){
             x: -2,
             y: 0,
         }
-        enemiesArr.push(new Enemy(x, y, width, height,"./imagenes/Cazador1.png", velocity, 1)) //
+        enemiesArr.push(new Enemy(x, y, width, height,"./imagenes/Cazador1.png", velocity, 1, "amarillo")) //
     },5000)
 }
-
 function generarEnemigoJeep(){
     setInterval(() =>{
         let width= 70
@@ -197,34 +218,47 @@ function generarEnemigoJeep(){
             x: -2,
             y: 0,
         }
-        //enemiesAmarillosArr.push(new Enemy(x, y, width, height,"./imagenes/Cazador1.jpeg", velocity, 5)) //""
-        //enemiesArr= [...enemiesAmarillosArr]
-        enemiesArr.push(new Enemy(x, y, width, height,"./imagenes/jeepCazador.png", velocity, 5)) //
+        enemiesArr.push(new Enemy(x, y, width, height,"./imagenes/jeepCazador.png", velocity, 5, "jeep")) 
     },5000)
 }
-
-
-//Variables para Animacion
+//funcio disparos automaticos Yeti
+function proyectilesYeti () {
+        /*let canvasRect= canvas.getBoundingClientRect()
+    setInterval(()=>{
+        let x = yeti1.x + yeti1.width/2 
+        let y = yeti1.y + yeti1.height/2 
+        const velocity= {
+              x: 5,
+              y: 0
+          }
+        proyectilesArr.push(new Proyectiles(x-5, y, "./imagenes/snowball.png", velocity))
+    },1000)
+    console.log(proyectilesArr)*/
+   
+   }
+////////////////////////
+//ANIMACION////////////
+///////////////////
 let animationId
 let frame = 0
 let scoreHunters = 0
 let gameTime = 0
+let coinSuma= 0
+let aldeanosSuma= 0
+let yetiVida= yeti1.vida
 function updateCanvas(){
-  requestAnimationFrame(updateCanvas)
+  animationId = requestAnimationFrame(updateCanvas)
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ground.draw()
+    groundGame.draw()
     yeti1.draw()
     bigFootArr.forEach((bigF) =>{
         bigF.draw()
     })
-    
     //const updateGameTime= (dt)=>{
      //   gameTime += dt
       //  console.log(gameTime)
     //}
-
     //timeHtml.innerText= updateGameTime()
-   
     particlesArray.forEach((particle, index) => {
         particle.updateParticles()
         if(particle.alpha <= 0){
@@ -235,17 +269,49 @@ function updateCanvas(){
     })
     proyectilesArr.forEach((proyect, index)=>{
         proyect.updateProyectil()
-        if (proyect.x-proyect.radio > canvas.width){           
+        if (proyect.x-proyect.width > canvas.width){           
                 proyectilesArr.splice(index, 1)
     }
     })
     enemiesArr.forEach((enemy, index)=>{
         enemy.draw()
         enemy.updateEnemy()
-
+        //EndGame
+        const distance= Math.hypot(yeti1.x-enemy.x, yeti1.y-enemy.y)
+        if(distance-yeti1.width-enemy.width <0.5) {
+            for(let i=0; i<8; i++){
+                particlesArray.push(new Particles(
+                    yeti1.x+40, 
+                    yeti1.y+30, 
+                    Math.random()*4, 
+                    "yellow", 
+                    {x:(Math.random()-0.5)*(Math.random()*3),
+                     y:(Math.random()-0.5)*(Math.random()*3)})) 
+            }
+            if(yeti1.vida>0) {
+                enemiesArr.splice(index, 1)
+                yeti1.vida-=1
+                yetiLiveCount.innerHTML= yeti1.vida
+            } else {
+                cancelAnimationFrame(animationId)
+                clearInterval()
+                groundLost.draw()
+                //gameOver.style.display="block"    
+            }
+        }
+        //Enemigos chocan y disminuyen vida Shooter    
         bigFootArr.forEach((bigF, bigFIndex)=>{
             const distance= Math.hypot(bigF.x-enemy.x, bigF.y-enemy.y)
             if(distance-bigF.width-enemy.width <0.5){
+                for(let i=0; i<8; i++){
+                    particlesArray.push(new Particles(
+                        bigF.x+40, 
+                        bigF.y+30, 
+                        Math.random()*4, 
+                        "white", 
+                        {x:(Math.random()-0.5)*(Math.random()*3),
+                         y:(Math.random()-0.5)*(Math.random()*3)})) 
+                }
                 if(bigF.vida >= 1){
                     enemiesArr.splice(index, 1)
                     bigF.vida=-1
@@ -256,33 +322,60 @@ function updateCanvas(){
             }    
         }
         })
-
-
+        //Daño, desaparicion de proyectiles y enemigos
         proyectilesArr.forEach((proyect, proyectIndex)=>{
-            const distance= Math.hypot(proyect.x-enemy.x, proyect.y-enemy.y)
-            if(distance-proyect.radio-enemy.width <0.5){
+            const distance= Math.hypot((proyect.x+proyect.width) - enemy.x, (proyect.y + proyect.width) - enemy.y)
+            if(distance-enemy.width <0.5){
+                //explota al chocar proyectil con enemigo
                 for(let i=0; i<8; i++){
                     particlesArray.push(new Particles(
                         proyect.x, 
                         proyect.y, 
                         Math.random()*2, 
-                        "red", 
+                        "gray", 
                         {x:(Math.random()-0.5)*(Math.random()*3),
                          y:(Math.random()-0.5)*(Math.random()*3)})) 
                 }
-                if(enemy.strength>=1){
+                if(enemy.strength>0){
                     enemy.strength-=1
                         proyectilesArr.splice(proyectIndex, 1)
-                   
                 }else{
-                    //aumenta conteo cazadores abatidos
+                    if(enemy.name==="jeep"){
+                        coinSuma+=5
+                        aldeanosSuma+=10  
+                        //aumenta conteo cazadores abatidos
                     scoreHunters+=1
-                    huntersScoreHTML.innerHTML = scoreHunters
                     //desaparece proy y enemigo al bajarle vida a 0
-                        enemiesArr.splice(index, 1)
-                        console.log(enemiesArr)
-                        proyectilesArr.splice(proyectIndex, 1)
-        
+                    enemiesArr.splice(index, 1)
+                    proyectilesArr.splice(proyectIndex, 1)
+                    coinsCountHtml.innerHTML= coinSuma
+                    aldeanosCountHtml.innerHTML= aldeanosSuma
+                    huntersScoreHTML.innerHTML = scoreHunters 
+                    }else if(enemy.name==="rojo"){
+                        coinSuma+=3
+                        aldeanosSuma+=7
+                        
+                        //aumenta conteo cazadores abatidos
+                    scoreHunters+=1
+                    //desaparece proy y enemigo al bajarle vida a 0
+                    enemiesArr.splice(index, 1)
+                    proyectilesArr.splice(proyectIndex, 1)
+                    huntersScoreHTML.innerHTML = scoreHunters 
+                    coinsCountHtml.innerHTML= coinSuma
+                    aldeanosCountHtml.innerHTML= aldeanosSuma
+                    }else{
+                        coinSuma+=1
+                        aldeanosSuma+=5
+                       
+                        //aumenta conteo cazadores abatidos
+                    scoreHunters+=1
+                    //desaparece proy y enemigo al bajarle vida a 0
+                    enemiesArr.splice(index, 1)
+                    proyectilesArr.splice(proyectIndex, 1)
+                    huntersScoreHTML.innerHTML = scoreHunters 
+                    coinsCountHtml.innerHTML= coinSuma
+                    aldeanosCountHtml.innerHTML= aldeanosSuma
+                    }
                 }
             }
         })
@@ -293,66 +386,83 @@ function updateCanvas(){
     frame++
 }
 
-//Comprar nuevos BigFoot
-//botonComprar.addEventListener("click", (event)=>{
-//})
-
-//se activan los disparadores
-        addEventListener ("click", (event)=>{
-            console.log(event.clientX, event.clientY)
-            let canvasRect= canvas.getBoundingClientRect()
-            for (let i=0; i<bigFootArr.length; i++){
-                if(event.clientX - canvasRect.left <= bigFootArr[i].x+bigFootArr[i].width && event.clientX -canvasRect.left > bigFootArr[i].x
-                    && event.clientY - canvasRect.top < bigFootArr[i].y + bigFootArr[i].height && event.clientY -canvasRect.top > bigFootArr[i].y){
-            const velocity= {
+//Activar Shooters
+addEventListener ("click", (event)=>{
+    console.log(event.clientX, event.clientY)
+    let canvasRect= canvas.getBoundingClientRect()
+    for (let i=0; i<bigFootArr.length; i++){
+        if(event.clientX - canvasRect.left <= bigFootArr[i].x+bigFootArr[i].width &&
+            event.clientX -canvasRect.left > bigFootArr[i].x &&
+            event.clientY - canvasRect.top < bigFootArr[i].y + bigFootArr[i].height &&
+            event.clientY -canvasRect.top > bigFootArr[i].y) 
+            {
+                const velocity= {
                 x: 4,
                 y: 0
-            }
-            proyectilesArr.push(new Proyectiles(bigFootArr[i].x,bigFootArr[i].y+35, 5, "red", velocity))
+                    }
+                proyectilesArr.push(new Proyectiles(bigFootArr[i].x+20,bigFootArr[i].y+35,"./imagenes/stone.png",velocity))
+}
+}
+})
+//Comprar nuevos BigFoot
+botonComprar.addEventListener("click", (event)=>{
+let x = xInicial + 90
+let y = yInicial + 5
+let width= 70
+let height= 70
+if(coinSuma>=1){
+    coinSuma-=1
+    coinsCountHtml.innerHTML= coinSuma
+    for(let i = 0; i < bigFootArr.length; i++){        
+        if(y == bigFootArr[i].y && y<=canvas.height+3){
+             y= bigFootArr[i].y + height
+             console.log(bigFootArr[i])
         }
     }
-        })
-        //let bigFoot1 = new Shooter(100, y, 70, 70)
-        //let bigFoot2 = new Shooter(100, y+150, 70, 70) 
-        console.log("viejo" + bigFootArr)
-    botonComprar.addEventListener("click", (event)=>{
-        let x = xInicial + 90
-        let y = yInicial + 5
-        let width= 70
-        let height= 70
-      for(let i = 0; i < bigFootArr.length; i++){        
-            if(y == bigFootArr[i].y){
-                 y= bigFootArr[i].y + height
-            }
-        }
-            bigFootArr.push(new Shooter(x, y, width, height)) 
-            console.log("nuevo"+bigFootArr)
-    })   
-    
-    console.log("nuevo2"+bigFootArr)
-
-   /* addEventListener("click", (event) =>{
-        const velocity= {
-            x: 4,
-            y: 0
-        }
-        for (let i=0; i<bigFootArr.length; i++){
-            
-            proyectilesArr.push(new Proyectiles(bigFootArr[i].x,bigFootArr[i].y+35, 5, "red", velocity))
-        }
-    })*/
-    //FUNCION PARA PROCESSAR IMAGENES
-    /*function init () {
-        updateCanvas()
-    }
-    resources.load([
-        "./images/yeti.png",
-    ]);
-    resources.onReady(init);*/
-    updateCanvas()
+        bigFootArr.push(new Shooter(x, y, width, height)) 
+        console.log("nuevo"+bigFootArr)
+}
+})   
+//Moviemiento Yeti Up and Down con flechas teclado
+//bloquear teclas izq y der
+/// revisar que no se mueva el scroll del browser
+addEventListener("keydown", event =>{
+if (event.defaultPrevented) {
+return; 
+}
+console.log(event.code)
+switch(event.code) {
+case "ArrowUp":
+    yeti1.moveUp();
+    break;
+case "ArrowDown":
+    yeti1.moveDown();
+    break;
+}
+})
+//Funcion para boton Start
+botonStart.addEventListener("click", () => {
+init()
+updateCanvas()
 generarEnemigoRojo()
 generarEnemigoAmarillo()
 generarEnemigoJeep()
+proyectilesYeti () 
+})
+//Cuando salga Game over convertir el boton Start en Restart o aparecer uno grandote en la parte alta de gameover
+/*const restartGame = restartGame.addEventListener("click", () => {
+init()
+updateCanvas()
+generarEnemigoRojo()
+generarEnemigoAmarillo()
+generarEnemigoJeep()
+proyectilesYeti () 
+gameOver.style.display="none"
+})*/
+//updateCanvas()
+//generarEnemigoRojo()
+//generarEnemigoAmarillo()
+//generarEnemigoJeep()
+//proyectilesYeti () 
 console.log(enemiesArr)
 console.log(particlesArray)
-
